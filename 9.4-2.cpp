8e8130197474976ca9bdf3b8b4c30a3126eb2d04
CC <caocancabbage@gmail.com>
2015-11-18 13:19:23 -0600

    9.4-2

---
+++ 9.4-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+using namespace std;
+// backtracking
+void findSubSets(vector<int> &set, int start, vector<int> &tmp, vector<vector<int>> &res) {
+    for (int i=start; i<set.size(); i++) {
+        tmp.push_back(set[i]);
+        res.push_back(tmp);
+        findSubSets(set, i+1, tmp, res);
+        tmp.pop_back();
+    }
+}
+
+vector<vector<int>> getvecSubsets(vector<int> &set) {
+    vector<vector<int>> res;
+    vector<int> tmp;
+    res.push_back(tmp);
+    findSubSets(set, 0, tmp, res);
+    return res;
+}
+
+
+
+
+
+
